d3f8613d46e2aafdd4e8ed1d1e2f7c917c0c710a
PetarTolev <petar.dtolev@gmail.com>
2019-10-31 23:17:35 +0200

    Added exercise

---
+++ Entity Framework Core/04. Code First - Exercise/P03_SalesDatabase/Data/SalesContext.cs
@@ -0,0 +1,92 @@
+﻿using System.ComponentModel;
+using P03_SalesDatabase.Data.Models;
+
+namespace P03_SalesDatabase.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class SalesContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(Config.ConnectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            ConfigureCustomerEntity(modelBuilder);
+
+            ConfigureProductEntity(modelBuilder);
+
+            ConfigureSaleEntity(modelBuilder);
+
+            ConfigureStoreEntity(modelBuilder);
+        }
+
+        private void ConfigureStoreEntity(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Store>()
+                .HasKey(s => s.StoreId);
+
+            modelBuilder
+                .Entity<Store>()
+                .Property(s => s.Name)
+                .HasMaxLength(80)
+                .IsUnicode();
+        }
+
+        private void ConfigureSaleEntity(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Sale>()
+                .HasKey(s => s.SaleId);
+
+            modelBuilder
+                .Entity<Sale>()
+                .HasOne(s => s.Product)
+                .WithMany(p => p.Sales);
+
+            modelBuilder
+                .Entity<Sale>()
+                .HasOne(s => s.Customer)
+                .WithMany(c => c.Sales);
+
+            modelBuilder
+                .Entity<Sale>()
+                .HasOne(s => s.Store)
+                .WithMany(s => s.Sales);
+        }
+
+        private void ConfigureProductEntity(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Product>()
+                .HasKey(p => p.ProductId);
+
+            modelBuilder
+                .Entity<Product>()
+                .Property(p => p.Name)
+                .HasMaxLength(50)
+                .IsUnicode();
+        }
+
+        private void ConfigureCustomerEntity(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Customer>()
+                .HasKey(c => c.CustomerId);
+
+            modelBuilder
+                .Entity<Customer>()
+                .Property(c => c.Name)
+                .HasMaxLength(100)
+                .IsUnicode();
+
+            modelBuilder
+                .Entity<Customer>()
+                .Property(c => c.Email)
+                .HasMaxLength(80);
+        }
+    }
+}
