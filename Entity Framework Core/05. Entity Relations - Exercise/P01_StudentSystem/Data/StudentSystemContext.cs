dea33e455a6b9d7ad6cd72cd260b79d087ce3935
PetarTolev <petar.dtolev@gmail.com>
2019-11-05 14:52:36 +0200

    Refactored exercise

--- Entity Framework Core/05. Entity Relations - Exercise/P01_StudentSystem/Data/StudentSystemContext.cs
+++ Entity Framework Core/05. Entity Relations - Exercise/P01_StudentSystem/Data/StudentSystemContext.cs
@@ -2,6 +2,7 @@
 {
     using Microsoft.EntityFrameworkCore;
     using Models;
+    using Configurations;
 
     public class StudentSystemContext : DbContext
     {
@@ -36,116 +37,17 @@
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            ConfigureStudentEntity(modelBuilder);
+            modelBuilder.ApplyConfiguration(new StudentConfiguration());
 
-            ConfigureCourseEntity(modelBuilder);
+            modelBuilder.ApplyConfiguration(new CourseConfiguration());
 
-            ConfigureResourceEntity(modelBuilder);
+            modelBuilder.ApplyConfiguration(new ResourceConfiguration());
 
-            ConfigureHomeworkEntity(modelBuilder);
+            modelBuilder.ApplyConfiguration(new HomeworkConfiguration());
 
-            ConfigureStudentCourseEntity(modelBuilder);
+            modelBuilder.ApplyConfiguration(new StudentCourseConfiguration());
 
             modelBuilder.Seed();
-        }
-
-        private void ConfigureStudentCourseEntity(ModelBuilder mb)
-        {
-            mb.Entity<StudentCourse>()
-                .HasKey(sc => new {sc.StudentId, sc.CourseId});
-        }
-
-        private void ConfigureHomeworkEntity(ModelBuilder modelBuilder)
-        {
-
-            modelBuilder.Entity<Homework>(homework =>
-            {
-                homework
-                    .HasKey(h => h.HomeworkId);
-
-                homework
-                    .Property(h => h.Content)
-                    .IsUnicode(false);
-            });
-        }
-
-        private void ConfigureResourceEntity(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Resource>(resource =>
-            {
-                resource
-                    .HasKey(r => r.ResourceId);
-
-                resource
-                    .Property(r => r.Name)
-                    .HasMaxLength(50)
-                    .IsUnicode();
-
-                resource
-                    .Property(r => r.Url)
-                    .IsUnicode(false);
-            });
-        }
-
-        private void ConfigureCourseEntity(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Course>(course =>
-            {
-                course
-                    .HasKey(c => c.CourseId);
-
-                course
-                     .HasMany(c => c.StudentsEnrolled)
-                     .WithOne(s => s.Course);
-
-                course
-                    .HasMany(c => c.Resources)
-                    .WithOne(r => r.Course);
-
-                course
-                    .HasMany(c => c.HomeworkSubmissions)
-                    .WithOne(h => h.Course);
-
-                course
-                    .Property(c => c.Name)
-                    .HasMaxLength(80)
-                    .IsUnicode()
-                    .IsRequired();
-
-                course
-                    .Property(c => c.Description)
-                    .IsRequired(false)
-                    .IsUnicode();
-            });
-        }
-
-        private void ConfigureStudentEntity(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Student>(student =>
-            {
-                student
-                    .HasKey(s => s.StudentId);
-
-                student
-                    .HasMany(s => s.CourseEnrollments)
-                    .WithOne(ce => ce.Student);
-
-                student
-                    .HasMany(s => s.HomeworkSubmissions)
-                    .WithOne(h => h.Student);
-
-                student
-                    .Property(s => s.Name)
-                    .HasMaxLength(100)
-                    .IsUnicode()
-                    .IsRequired();
-
-                student
-                    .Property(s => s.PhoneNumber)
-                    .HasColumnType("CHAR(10)")
-                    .IsUnicode(false)
-                    .IsRequired(false);
-            });
         }
     }
 }
--- Entity Framework Core/05. Entity Relations - Exercise/P03_FootballBetting/Data/FootballBettingContext.cs
+++ Entity Framework Core/05. Entity Relations - Exercise/P03_FootballBetting/Data/FootballBettingContext.cs
@@ -2,6 +2,7 @@
 {
     using Microsoft.EntityFrameworkCore;
     using Models;
+    using Configurations;
 
     public class FootballBettingContext : DbContext
     {
@@ -46,183 +47,25 @@
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            ConfigureBetEntity(modelBuilder);
+            modelBuilder.ApplyConfiguration(new BetConfiguration());
+            
+            modelBuilder.ApplyConfiguration(new ColorConfiguration());
 
-            ConfigureColorEntity(modelBuilder);
+            modelBuilder.ApplyConfiguration(new CountryConfiguration());
 
-            ConfigureCountryEntity(modelBuilder);
+            modelBuilder.ApplyConfiguration(new GameConfiguration());
 
-            ConfigureGameEntity(modelBuilder);
+            modelBuilder.ApplyConfiguration(new PlayerConfiguration());
 
-            ConfigurePlayerEntity(modelBuilder);
+            modelBuilder.ApplyConfiguration(new PlayerStatisticConfiguration());
 
-            ConfigurePlayerStatisticEntity(modelBuilder);
+            modelBuilder.ApplyConfiguration(new PositionConfiguration());
 
-            ConfigurePositionEntity(modelBuilder);
+            modelBuilder.ApplyConfiguration(new TeamConfiguration());
 
-            ConfigureTeamEntity(modelBuilder);
+            modelBuilder.ApplyConfiguration(new TownConfiguration());
 
-            ConfigureTownEntity(modelBuilder);
-
-            ConfigureUserEntity(modelBuilder);
-        }
-
-        private void ConfigureUserEntity(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<User>(user =>
-            {
-                user.HasKey(u => u.UserId);
-
-                user
-                    .HasMany(u => u.Bets)
-                    .WithOne(b => b.User);
-            });
-        }
-
-        private void ConfigureTownEntity(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Town>(town =>
-            {
-                town.HasKey(t => t.TownId);
-
-                town
-                    .HasMany(to => to.Teams)
-                    .WithOne(te => te.Town);
-            });
-        }
-
-        private void ConfigureTeamEntity(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Team>(team =>
-            {
-                team.HasKey(t => t.TeamId);
-
-                team
-                    .HasMany(t => t.Players)
-                    .WithOne(p => p.Team);
-
-                team
-                    .HasMany(t => t.HomeGames)
-                    .WithOne(g => g.HomeTeam)
-                    .OnDelete(DeleteBehavior.Restrict);
-
-                team
-                    .HasMany(t => t.AwayGames)
-                    .WithOne(g => g.AwayTeam)
-                    .OnDelete(DeleteBehavior.Restrict);
-
-                team
-                    .Property(t => t.Name)
-                    .IsRequired();
-            });
-        }
-
-        private void ConfigurePositionEntity(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Position>(position =>
-            {
-                position
-                    .HasKey(p => p.PositionId);
-
-                position
-                    .HasMany(po => po.Players)
-                    .WithOne(pi => pi.Position);
-
-                position
-                    .Property(p => p.Name)
-                    .IsRequired();
-            });
-        }
-
-        private void ConfigurePlayerStatisticEntity(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<PlayerStatistic>(playerStatistic =>
-            {
-                playerStatistic.HasKey(ps => new {ps.PlayerId, ps.GameId});
-            });
-        }
-
-        private void ConfigurePlayerEntity(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Player>(player =>
-            {
-                player
-                    .HasKey(p => p.PlayerId);
-
-                player
-                    .HasMany(p => p.PlayerStatistics)
-                    .WithOne(ps => ps.Player);
-
-                player
-                    .Property(p => p.Name)
-                    .IsRequired();
-            });
-        }
-
-        private void ConfigureGameEntity(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Game>(game =>
-            {
-                game
-                    .HasKey(g => g.GameId);
-
-                game
-                    .HasMany(g => g.PlayerStatistics)
-                    .WithOne(ps => ps.Game);
-
-                game
-                    .HasOne(g => g.HomeTeam)
-                    .WithMany(ht => ht.HomeGames);
-            });
-        }
-
-        private void ConfigureCountryEntity(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Country>(country =>
-            {
-                country
-                    .HasKey(c => c.CountryId);
-
-                country
-                    .HasMany(c => c.Towns)
-                    .WithOne(t => t.Country);
-            });
-        }
-
-        private void ConfigureColorEntity(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Color>(color =>
-            {
-                color.HasKey(c => c.ColorId);
-
-                color
-                    .HasMany(c => c.PrimaryKitTeams)
-                    .WithOne(t => t.PrimaryKitColor)
-                    .OnDelete(DeleteBehavior.Restrict);
-
-                color
-                    .HasMany(c => c.SecondaryKitTeams)
-                    .WithOne(t => t.SecondaryKitColor)
-                    .OnDelete(DeleteBehavior.Restrict);
-            });
-        }
-
-        private void ConfigureBetEntity(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Bet>(bet =>
-            {
-                bet
-                    .HasKey(b => b.BetId);
-
-                bet
-                    .Property(b => b.Prediction)
-                    .IsRequired();
-
-                bet
-                    .Property(b => b.Amount)
-                    .HasColumnType("MONEY")
-                    .IsRequired();
-            });
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
         }
     }
 }
