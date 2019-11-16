02a17bcee3f1cc1ea3ea8a40d4c9d29f28cb43b8
PetarTolev <petar.dtolev@gmail.com>
2019-11-16 22:47:40 +0200

    Added exercise

---
+++ Entity Framework Core/09. XML - Exercise/CarDealer/CarDealer/Dtos/Import/ImportSaleDto.cs
@@ -0,0 +1,17 @@
+﻿namespace CarDealer.Dtos.Import
+{
+    using System.Xml.Serialization;
+
+    [XmlType("Sale")]
+    public class ImportSaleDto
+    {
+        [XmlElement("carPartIdDto")]
+        public int CarId { get; set; }
+
+        [XmlElement("customerPartIdDto")]
+        public int CustomerId { get; set; }
+
+        [XmlElement("discount")]
+        public int Discount { get; set; }
+    }
+}
