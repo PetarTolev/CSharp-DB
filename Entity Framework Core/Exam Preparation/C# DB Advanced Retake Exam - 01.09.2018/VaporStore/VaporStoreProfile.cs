fa0d33791a8034fc921b0cf5b331da813a722b46
PetarTolev <petar.dtolev@gmail.com>
2019-11-18 16:27:51 +0200

    Added exam preparation

---
+++ Entity Framework Core/Exam Preparation/C# DB Advanced Retake Exam - 01.09.2018/VaporStore/VaporStoreProfile.cs
@@ -0,0 +1,16 @@
+﻿namespace VaporStore
+{
+    using AutoMapper;
+    using Data.Models;
+    using DataProcessor.Dto.Import;
+
+    public class VaporStoreProfile : Profile
+	{
+		// Configure your AutoMapper here if you wish to use it. If not, DO NOT DELETE THIS CLASS
+		public VaporStoreProfile()
+        {
+            this.CreateMap<GameImportDto, Game>();
+            this.CreateMap<Game, GameImportDto>();
+        }
+	}
+}
